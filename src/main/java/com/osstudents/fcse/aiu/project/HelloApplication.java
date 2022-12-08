c3176df7053f213e415fb4ab2e02bb762303deee
SpectreWasTaken <youssef.bedair@aiu.edu.eg>
2022-12-08 20:04:40 +0200

    Added Threads, Removed Bloat

--- src/main/java/com/osstudents/fcse/aiu/project/HelloApplication.java
+++ src/main/java/com/osstudents/fcse/aiu/project/HelloApplication.java
@@ -8,6 +8,10 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    public static final String[][] Dictionary = {{}};
+    public static String[][] getDic(){
+        return Dictionary;
+    }
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
--- src/main/java/com/osstudents/fcse/aiu/project/ThreadforDataintre.java
+++
@@ -1,7 +0,0 @@
-package com.osstudents.fcse.aiu.project;
-
-public class ThreadforDataintre extends Thread {
-    public void run(){
-
-    }
-}
