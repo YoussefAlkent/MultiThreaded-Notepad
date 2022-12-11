e6c4c333c2032984b373cb99254f1f11836d2178
SpectreWasTaken <youssef.bedair@aiu.edu.eg>
2022-12-11 12:13:28 +0200

    V1.0, Final Release

--- src/main/java/com/osstudents/fcse/aiu/project/EditorApp.java
+++ src/main/java/com/osstudents/fcse/aiu/project/EditorApp.java
@@ -14,8 +14,8 @@ import java.util.Scanner;
 
 public class EditorApp extends Application {
     public static String[][] Dictionary = new String[90][2];
-    private static void initDictionary() throws FileNotFoundException {
-        File file = new File("dictionary.txt");
+    public static void initDictionary() throws FileNotFoundException {
+        File file = new File("C:\\Users\\Spectre\\IdeaProjects\\Project\\src\\main\\java\\com\\osstudents\\fcse\\aiu\\project\\dictionary.txt");
         Scanner fileReader = new Scanner(file);
         int i = 0;
         while(fileReader.hasNext()){
@@ -28,9 +28,9 @@ public class EditorApp extends Application {
     }
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("test.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Editor.fxml"));
         //loader.setBuilderFactory(t -> (Builder<?>) new EditorController(new EditorModel()));
-        Scene scene = new Scene(loader.load(), 320, 240);
+        Scene scene = new Scene(loader.load(), 600, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/osstudents/fcse/aiu/project/EditorController.java
+++ src/main/java/com/osstudents/fcse/aiu/project/EditorController.java
@@ -1,26 +1,86 @@
 package com.osstudents.fcse.aiu.project;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.FileChooser;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
-public  class EditorController {
+public class EditorController {
+    public Label statusMessage;
+    public Button cleanText;
+    private File textFile;
     @FXML
-    private TextArea areaText;
-    @FXML
-    private void onSave() {
+    private TextArea MainArea;
 
+    @FXML
+    private void chooseFile(ActionEvent event) throws FileNotFoundException {
+        String text = "";
+        FileChooser fileChooser = new FileChooser();
+        textFile = fileChooser.showOpenDialog(null);
+        if (textFile != null) {
+            Scanner file = new Scanner(textFile);
+            while (file.hasNext()) {
+                text += file.nextLine();
+            }
+        }
+        MainArea.setText(text);
     }
-    @FXML
-    private void onLode() {
 
-    }
     @FXML
-    private void onClose() {
-    System.exit(0);
+    private void saveFile(ActionEvent event) throws IOException {
+        if (textFile != null) {
+            FileWriter writer = new FileWriter(textFile);
+            writer.write(MainArea.getText());
+            writer.flush();
+            return;
+        }
+        FileChooser chooser = new FileChooser();
+        File newFile = chooser.showSaveDialog(null);
+        FileWriter writer = new FileWriter(newFile);
+        writer.write(MainArea.getText());
+        writer.flush();
     }
-    @FXML
-    private void onAbout() {
 
+    @FXML
+    private void onTextEdit(KeyEvent event) throws InterruptedException, FileNotFoundException {
+        Thread spaceDetection = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String fullText = MainArea.getText();
+                int caretPosition = MainArea.getCaretPosition();
+                fullText = fullText.replaceAll("[ ]{2,}", " ");
+                MainArea.setText(fullText);
+                MainArea.positionCaret(caretPosition);
+
+            }
+        });
+        spaceDetection.start();
+        spaceDetection.join();
+        EditorApp.initDictionary();
+        Thread wordCorrection = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int caretPos = MainArea.getCaretPosition();
+                String ArrayPass = MainArea.getText();
+                String[][] Dictionary = EditorApp.getDic();
+                for (int i = 0; i < Dictionary.length-5; i++) {
+                    System.out.println(Dictionary[i][0]);
+                    ArrayPass = ArrayPass.replaceAll(Dictionary[i][0], Dictionary[i][1]);
+                }
+                MainArea.setText(ArrayPass);
+                MainArea.positionCaret(caretPos+1);
+            }
+        });
+        wordCorrection.start();
+        wordCorrection.join();
     }
 
 }
